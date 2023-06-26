afc5095ccdf056b31cf95d4459d478e55846e8c6
vinctuan02 <vinctuan02@gmail.com>
2023-06-26 09:39:27 +0700

    oke

---
+++ src/configs/connectDB.js
@@ -0,0 +1,15 @@
+import mysql from 'mysql2/promise';
+
+// // create the connection to database
+
+console.log("Creating connection pool...");
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejsbasic',
+    // password: 'password'
+})
+
+
+export default pool;
